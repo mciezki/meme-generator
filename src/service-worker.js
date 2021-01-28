ab4878485acb2229363d9ed2f37bdcd57b63b3dc
Michał Ciężki <m.ciezky97@gmail.com>
2021-01-29 00:37:08 +0100

    previous firebase

--- src/service-worker.js
+++ src/service-worker.js
@@ -76,13 +76,14 @@ importScripts('https://www.gstatic.com/firebasejs/8.2.4/firebase-messaging.js');
 
 
 var firebaseConfig = {
-  apiKey: "AIzaSyAGKXGsU3TJzmpiYxXiou0b78KPxjLVEGc",
-  authDomain: "meme-generator3.firebaseapp.com",
-  databaseURL: "https://meme-generator3-default-rtdb.europe-west1.firebasedatabase.app/",
-  projectId: "meme-generator3",
-  storageBucket: "meme-generator3.appspot.com",
-  messagingSenderId: "852828033097",
-  appId: "1:852828033097:web:60e92d515f464ffdd1af98"
+  apiKey: "AIzaSyA5kSJGlUmKC9PftAV8LyHcpdjoaOwKbJ8",
+  authDomain: "meme-generator-fe1c4.firebaseapp.com",
+  databaseURL: "https://meme-generator-fe1c4-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "meme-generator-fe1c4",
+  storageBucket: "meme-generator-fe1c4.appspot.com",
+  messagingSenderId: "888796383810",
+  appId: "1:888796383810:web:657b8ecffc3740a026fbdb",
+  measurementId: "G-8007NWQCGB"
 };
 
 firebase.initializeApp(firebaseConfig);
